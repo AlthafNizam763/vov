6642a050bae8e33ff2785470328b8132c9e443e1
althaf <althaf@gmail.com>
2025-11-10 16:44:57 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.voiceofthevoiceless.co.in',
+  siteUrl: "https://www.voiceofthevoiceless.co.in",
   generateRobotsTxt: true,
-  sitemapSize: 7000,
-  exclude: ['/404'],
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  },
+  sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
 };
