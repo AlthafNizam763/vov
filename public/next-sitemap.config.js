49f59b8a9e8b4d90dc5a270ad5c1f34f82abcd0b
althaf <althaf@gmail.com>
2025-11-10 16:23:31 +0530

    Seo

---
+++ public/next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.voiceofthevoiceless.co.in',
+  generateRobotsTxt: true,
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,13 +13,74 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Voice of Voiceless",
-  description: "Let’s donate now to help fellow humans in need.",
+  title: "Voice of the Voiceless | Empowering the Unheard",
+  description:
+    "Voice of the Voiceless is a platform dedicated to helping those in need through compassion, awareness, and action. Join us to make a difference.",
+  keywords: [
+    "Voice of the Voiceless",
+    "NGO",
+    "charity",
+    "Kerala",
+    "social work",
+    "donation",
+    "humanitarian",
+    "volunteering",
+  ],
+  openGraph: {
+    title: "Voice of the Voiceless",
+    description:
+      "Let’s donate now to help fellow humans in need. Together, we can give voice to the voiceless.",
+    url: "https://www.voiceofthevoiceless.co.in",
+    siteName: "Voice of the Voiceless",
+    images: [
+      {
+        url: "https://www.voiceofthevoiceless.co.in/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Voice of the Voiceless Banner",
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Voice of the Voiceless",
+    description:
+      "Join us in empowering the unheard through compassion and action.",
+    images: ["https://www.voiceofthevoiceless.co.in/og-image.png"],
+  },
+  metadataBase: new URL("https://www.voiceofthevoiceless.co.in"),
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Structured Data for SEO (Organization Schema) */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Voice of the Voiceless",
+              url: "https://www.voiceofthevoiceless.co.in",
+              logo: "https://www.voiceofthevoiceless.co.in/vov-logo.png",
+              sameAs: [
+                "https://www.facebook.com/voice__of_the__voiceless",
+                "https://www.instagram.com/voice__of_the__voiceless",
+                "https://www.youtube.com/@voice__of_the__voiceless",
+              ],
+              description:
+                "Voice of the Voiceless is a humanitarian initiative to support people in need through awareness, aid, and action.",
+            }),
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
